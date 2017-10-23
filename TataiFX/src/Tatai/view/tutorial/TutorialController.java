ba69d94028f049bba00742e6fca852619d07eeaf
Kevin Manson <Kevin Manson@DESKTOP-SDTI0TU>
2017-10-23 18:10:34 +1300

    Finished help for Game Basics.

--- TataiFX/src/Tatai/view/tutorial/TutorialController.java
+++ TataiFX/src/Tatai/view/tutorial/TutorialController.java
@@ -73,11 +73,16 @@ public class TutorialController implements Initializable {
     private boolean btnMathsModePressed;
     private boolean btnAboutTataiPressed;
     
+    /* Images for Game Basics */
 	private final Image NowPlaying = new Image(getClass().getResourceAsStream("/Tatai/resources/GameBasics/NowPlaying.png"));
 	private final Image Player = new Image(getClass().getResourceAsStream("/Tatai/resources/GameBasics/Player.png"));
 	private final Image QuestionScore = new Image(getClass().getResourceAsStream("/Tatai/resources/GameBasics/QuestionScore.png"));
 	private final Image EndGame = new Image(getClass().getResourceAsStream("/Tatai/resources/GameBasics/EndGame.png"));
 	private final Image Instructions = new Image(getClass().getResourceAsStream("/Tatai/resources/GameBasics/Instructions.png"));
+	private final Image PlayRecording = new Image(getClass().getResourceAsStream("/Tatai/resources/GameBasics/PlayRecording.png"));
+	private final Image TryAgain = new Image(getClass().getResourceAsStream("/Tatai/resources/GameBasics/TryAgain.png"));
+	private final Image CorrectAnswer = new Image(getClass().getResourceAsStream("/Tatai/resources/GameBasics/CorrectAnswer.png"));
+	private final Image Pronunciation = new Image(getClass().getResourceAsStream("/Tatai/resources/GameBasics/Pronunciation.png"));
 
 	/**
 	 * Initializes the buttons on the screen
@@ -85,6 +90,7 @@ public class TutorialController implements Initializable {
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		lblUser.setText("Logged in as: " + LoginController.getCurrentPlayer());
+		lblImage.setText("1/9");
 		JFXDepthManager.setDepth(cardPane,  4);
 		JFXDepthManager.setDepth(topPane, 5);
 		
@@ -116,23 +122,50 @@ public class TutorialController implements Initializable {
 	@FXML
 	private void btnModeHandler(ActionEvent event) throws IOException {
 		if (event.getSource().equals(btnGameBasics)) {
+			image.setVisible(true);
+			btnNext.setVisible(true);
+			btnPrev.setVisible(true);
+			lblImage.setVisible(true);
+			
+			lblImage.setText("1/9");
+			image.setImage(NowPlaying);
+			
 			removeButtonStyles();
 			btnGameBasics.getStyleClass().add("pressed");
+			
 			btnGameBasicsPressed = true;
 			
 			Image selectMode = new Image(getClass().getResourceAsStream("/Tatai/resources/NowPlaying.png"));
 			image.setImage(selectMode);
 		} else if (event.getSource().equals(btnPracticeMode)) {
+			image.setVisible(true);
+			btnNext.setVisible(true);
+			btnPrev.setVisible(true);
+			lblImage.setVisible(true);
+			
 			removeButtonStyles();
 			btnPracticeMode.getStyleClass().add("pressed");
+			
 			btnPracticeModePressed = true;
 		} else if (event.getSource().equals(btnMathsMode)) {
+			image.setVisible(true);
+			btnNext.setVisible(true);
+			btnPrev.setVisible(true);
+			lblImage.setVisible(true);
+			
 			removeButtonStyles();
-			btnMathsMode.getStyleClass().add("pressed");		
+			btnMathsMode.getStyleClass().add("pressed");	
+			
 			btnMathsModePressed = true;
 		} else if (event.getSource().equals(btnAboutTatai)) {
+			image.setVisible(false);
+			btnNext.setVisible(false);
+			btnPrev.setVisible(false);
+			lblImage.setVisible(false);
+			
 			removeButtonStyles();
 			btnAboutTatai.getStyleClass().add("pressed");	
+			
 			btnAboutTataiPressed = true;
 		}
 	}
@@ -144,16 +177,28 @@ public class TutorialController implements Initializable {
 		if (btnGameBasicsPressed) {
 			if (image.getImage().equals(NowPlaying)) {
 				image.setImage(Player);
-				lblImage.setText("2/10");
+				lblImage.setText("2/9");
 			} else if (image.getImage().equals(Player)) {
 				image.setImage(QuestionScore);
-				lblImage.setText("3/10");
+				lblImage.setText("3/9");
 			} else if (image.getImage().equals(QuestionScore)) {
 				image.setImage(EndGame);
-				lblImage.setText("4/10");
+				lblImage.setText("4/9");
 			} else if (image.getImage().equals(EndGame)) {
 				image.setImage(Instructions);
-				lblImage.setText("5/10");
+				lblImage.setText("5/9");
+			} else if (image.getImage().equals(Instructions)) {
+				image.setImage(PlayRecording);
+				lblImage.setText("6/9");
+			} else if (image.getImage().equals(PlayRecording)) {
+				image.setImage(TryAgain);
+				lblImage.setText("7/9");
+			} else if (image.getImage().equals(TryAgain)) {
+				image.setImage(CorrectAnswer);
+				lblImage.setText("8/9");
+			} else if (image.getImage().equals(CorrectAnswer)) {
+				image.setImage(Pronunciation);
+				lblImage.setText("9/9");
 			}
 		}
 	}
@@ -163,18 +208,30 @@ public class TutorialController implements Initializable {
 	private void btnPrevHandler(ActionEvent event) throws IOException {
 		//if we are currently on the Game Basics tab
 		if (btnGameBasicsPressed) {
-			if (image.getImage().equals(Instructions)) {
+			if (image.getImage().equals(Pronunciation)) {
+				image.setImage(CorrectAnswer);
+				lblImage.setText("8/9");
+			} else if (image.getImage().equals(CorrectAnswer)) {
+				image.setImage(TryAgain);
+				lblImage.setText("7/9");
+			} else if (image.getImage().equals(TryAgain)) {
+				image.setImage(PlayRecording);
+				lblImage.setText("6/9");
+			} else if (image.getImage().equals(PlayRecording)) {
+				image.setImage(Instructions);
+				lblImage.setText("5/9");
+			} else if (image.getImage().equals(Instructions)) {
 				image.setImage(EndGame);
-				lblImage.setText("4/10");
+				lblImage.setText("4/9");
 			} else if (image.getImage().equals(EndGame)) {
 				image.setImage(QuestionScore);
-				lblImage.setText("3/10");
+				lblImage.setText("3/9");
 			} else if (image.getImage().equals(QuestionScore)) {
 				image.setImage(Player);
-				lblImage.setText("2/10");
+				lblImage.setText("2/9");
 			} else if (image.getImage().equals(Player)) {
 				image.setImage(NowPlaying);
-				lblImage.setText("1/10");
+				lblImage.setText("1/9");
 			}
 		}
 	}
